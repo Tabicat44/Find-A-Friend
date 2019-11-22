976add60711b9ee4400a1a4cef0071f3daf903e1
Tabitha <Tabiernst@Yahoo.com>
2019-11-22 15:17:01 -0500

    Added Fragments and Entrance Thymeleafs

---
+++ src/main/java/org/launchcode/findafriend/FindAFriendApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.findafriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindAFriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindAFriendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/findafriend/FindAFriendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.findafriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindAFriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
